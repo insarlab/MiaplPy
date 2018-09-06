1023fab6204b0da9e71c47eaa5f316c567fef01a
Sara Mirzaee <37273875+mirzaees@users.noreply.github.com>
2018-09-06 08:39:23 -0400

    Update stackSentinel_squeesar.py

--- stackSentinel_squeesar.py
+++ stackSentinel_squeesar.py
@@ -5,7 +5,7 @@
 #######################
 
 import os, imp, sys, glob, fnmatch
-pathh = os.environ['PARENTDIR']'+'/sources/sentinelStack'
+pathh = os.environ['PARENTDIR']+'/sources/sentinelStack'
 sys.path.append(pathh)
 import argparse
 import configparser
