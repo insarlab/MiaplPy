182a1ff1afc615d9322c8da71bc407b8bb116726
Sara Mirzaee <37273875+mirzaees@users.noreply.github.com>
2018-10-29 16:26:18 -0400

    Create run_sequential.py

---
+++ run_sequential.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author Sara Mirzaee
+
+import os
+import sys
+import subprocess
+import argparse
+from datetime import datetime
+import shutil
+import time
+import glob
+
+import generate_templates as gt
+
+import logging
