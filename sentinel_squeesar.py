6fe1969b3579238a1552ffa0c02c1769a0a3689c
Sara Mirzaee <37273875+mirzaees@users.noreply.github.com>
2018-11-20 22:10:11 -0500

    Update sentinel_squeesar.py

--- sentinel_squeesar.py
+++ sentinel_squeesar.py
@@ -148,7 +148,7 @@ def main(iargs=None):
     
     if flag == 'patchlist_created' and PSQ == True:
         #cmd = 'createBatch.pl ' + inps.sq_dir + '/run_PSQ_sentinel' + ' memory=' + '3700' + ' walltime=' + '10:00'
-        cmd = '$INT_SCR/split_jobs.py -f ' + inps.sq_dir + '/run_PSQ_sentinel -w 10:00 -r 3000 -q '+ jobqueue 
+        cmd = '$INT_SCR/split_jobs.py -f ' + inps.sq_dir + '/run_PSQ_sentinel -w 5:00 -r 3700 -q '+ jobqueue 
         status = subprocess.Popen(cmd, shell=True).wait()
         print(status)
         if status is not 0:
