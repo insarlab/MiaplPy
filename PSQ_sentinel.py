46b74e02004d6d6366a6bd1588845a5b70eabe28
Sara Mirzaee <37273875+mirzaees@users.noreply.github.com>
2018-11-21 11:02:50 -0500

    Update PSQ_sentinel.py

--- PSQ_sentinel.py
+++ PSQ_sentinel.py
@@ -80,8 +80,8 @@ def main(iargs=None):
 
     inps.project_name = os.path.basename(inps.custom_template_file).partition('.')[0]
     inps.project_dir = os.getenv('SCRATCHDIR') + '/' + inps.project_name
-    inps.template = Template(inps.custom_template_file).get_options()
     inps.scratch_dir = os.getenv('SCRATCHDIR')
+    
     inps.slave_dir = inps.project_dir + '/merged/SLC'
     inps.sq_dir = inps.project_dir + '/SqueeSAR'
     inps.list_slv = os.listdir(inps.slave_dir)
@@ -99,76 +99,10 @@ def main(iargs=None):
     rslc = np.memmap(inps.work_dir + '/RSLC', dtype=np.complex64, mode='r', shape=(inps.n_image, inps.lin, inps.sam))
 
 
-    inps.range_win = int(inps.template['squeesar.wsizerange'])
-    inps.azimuth_win = int(inps.template['squeesar.wsizeazimuth'])
+    inps.range_win = int(Template(inps.custom_template_file).get_options()['squeesar.wsizerange'])
+    inps.azimuth_win = int(Template(inps.custom_template_file).get_options()['squeesar.wsizeazimuth'])
 
-    ################### Finding Statistical homogeneous pixels ################
-    num_slc = 20    # to find SHPs only
-    if not os.path.isfile(inps.work_dir + '/SHP.pkl'):
-        
-        time0 = time.time()
-        shp_df = pd.DataFrame(columns=['ref_pixel','rows','cols','amp_ref','phase_ref'])
-        lin = np.ogrid[0:inps.lin]
-        sam = np.ogrid[0:inps.sam]
-        lin, sam = np.meshgrid(lin, sam)
-        coords = list(map(lambda x, y: (int(x), int(y)),
-                     lin.T.reshape(inps.lin*inps.sam, 1), sam.T.reshape(inps.lin*inps.sam, 1)))
-        del lin, sam
-
-        for coord in coords:
-
-            lin,sam = coord
-            r = np.ogrid[lin - ((inps.azimuth_win - 1) / 2):lin + ((inps.azimuth_win - 1) / 2) + 1]
-            ref_row = np.array([(inps.azimuth_win - 1) / 2])
-            r = r[r >= 0]
-            r = r[r < inps.lin]
-            ref_row = ref_row - (inps.azimuth_win - len(r))
-            c = np.ogrid[sam - ((inps.range_win - 1) / 2):sam + ((inps.range_win - 1) / 2) + 1]
-            ref_col = np.array([(inps.range_win - 1) / 2])
-            c = c[c >= 0]
-            c = c[c < inps.sam]
-            ref_col = ref_col - (inps.range_win - len(c))
-
-            x, y = np.meshgrid(r.astype(int), c.astype(int), sparse=True)
-            win = np.abs(rslc[0:num_slc, x, y])
-            win = pysq.trwin(win)
-
-            test_vec = win.reshape(num_slc, len(r) * len(c))
-            ks_res = np.zeros(len(r) * len(c))
-            ref_scatterer = np.abs(rslc[0:num_slc, lin, sam])
-            ref_scatterer = ref_scatterer.reshape(num_slc, 1)
-            for pixel in range(len(test_vec[0])):
-                sample_scatterer = test_vec[:, pixel]
-                sample_scatterer = sample_scatterer.reshape(num_slc, 1)
-
-                try:
-                    test = anderson_ksamp([ref_scatterer, sample_scatterer])
-                    if test.significance_level > 0.05:
-                        ks_res[pixel] = 1
-                    else:
-                        ks_res[pixel] = 0
-                except:
-                    ks_res[pixel] = 0
-
-            ks_result = ks_res.reshape(len(r), len(c))
-            ks_label = label(ks_result, background=False, connectivity=2)
-            ref_label = ks_label[ref_row.astype(int), ref_col.astype(int)]
-
-            rr, cc = np.where(ks_label == ref_label)
-
-            rr = rr + r[0]
-            cc = cc + c[0]
-            if len(rr) > 20:
-                shp = {'ref_pixel': [lin, sam], 'rows': rr, 'cols': cc, 'amp_ref':{}, 'phase_ref':{}}
-                shp_df = shp_df.append(shp, ignore_index=True)
-
-        shp_df.to_pickle(inps.work_dir + '/SHP.pkl')
-        
-        timep = time.time() - time0
-        print('time spent to find SHPs {}: min'.format(timep/60))
-    else:
-        print('SHP Exists...')
-
+    
     ###################### Sequential Phase linking ###############################
     
 
