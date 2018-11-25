eb46c279bc3026f3d56d050a3f307903545c48c8
Sara Mirzaee <37273875+mirzaees@users.noreply.github.com>
2018-11-25 17:43:54 -0500

    Update find_shp.py

--- find_shp.py
+++ find_shp.py
@@ -127,7 +127,7 @@ def main(iargs=None):
             rr = rr + r[0]
             cc = cc + c[0]
             if len(rr) > 20:
-                shp = {'ref_pixel': np.uint16([lin, sam]), 'rows': np.uin16(rr), 'cols': np.uint16(cc), 'amp_ref':{}, 'phase_ref':{}}
+                shp = {'ref_pixel': np.uint16([lin, sam]), 'rows': np.uint16(rr), 'cols': np.uint16(cc), 'amp_ref':{}, 'phase_ref':{}}
                 shp_df = shp_df.append(shp, ignore_index=True)
 
         shp_df.to_pickle(inps.work_dir + '/SHP.pkl')
