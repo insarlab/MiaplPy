7dc1241701aac48c7cda0092db3645d211633ea7
Sara Mirzaee <sxm1611@login4.pegasus.edu>
2018-12-22 10:58:58 -0500

    Updated

--- sentinel_squeesar.py
+++ sentinel_squeesar.py
@@ -92,12 +92,12 @@ def main(iargs=None):
     inps.sq_dir = inps.project_dir + '/SqueeSAR'
     inps.patch_dir = inps.sq_dir+'/PATCH'
     inps.list_slv = os.listdir(inps.slave_dir)
-    A = [inps.list_slv[0]+'_'+x for x in inps.list_slv]
-    with open('A.txt','w') as f:
-        for t in A[1::]:
-            f.write(t+'\n')
+    #A = [inps.list_slv[0]+'_'+x for x in inps.list_slv]
+    #with open('A.txt','w') as f:
+    #    for t in A[1::]:
+    #        f.write(t+'\n')
     
-    sys.exit(1) 
+    #sys.exit(1) 
     
     inps.range_win = int(Template(inps.custom_template_file).get_options()['squeesar.wsizerange'])
     inps.azimuth_win = int(Template(inps.custom_template_file).get_options()['squeesar.wsizeazimuth'])
@@ -148,11 +148,11 @@ def main(iargs=None):
                     cmd = 'PSQ_sentinel.py ' + inps.custom_template_file + ' -p ' + 'PATCH' + patch + ' \n'
                     f.write(cmd)
         ## cmd = 'createBatch.pl ' + inps.sq_dir + '/run_PSQ_sentinel' + ' memory=' + '3700' + ' walltime=' + '10:00'
-        cmd = 'submit_jobs.py -f ' + inps.sq_dir + '/run_PSQ_sentinel -w 3:00 -r 3000 -q ' + jobqueue
-        status = subprocess.Popen(cmd, shell=True).wait()
-        if status is not 0:
-            #logger_ph_lnk.log(loglevel.ERROR, 'ERROR running PSQ_sentinel.py')
-            raise Exception('ERROR running PSQ_sentinel.py')
+        #cmd = 'submit_jobs.py -f ' + inps.sq_dir + '/run_PSQ_sentinel -w 3:00 -r 3000 -q ' + jobqueue
+        #status = subprocess.Popen(cmd, shell=True).wait()
+        #if status is not 0:
+        #    #logger_ph_lnk.log(loglevel.ERROR, 'ERROR running PSQ_sentinel.py')
+        #    raise Exception('ERROR running PSQ_sentinel.py')
 
  ###########################################   
 
