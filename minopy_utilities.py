fix negative sign

minopy_utilities.py
@@ -235,8 +235,8 @@ def PTA_L_BFGS(coh0):
         out = res.x.reshape(n, 1)
         vec = np.multiply(np.abs(x0), np.exp(1j * out)).reshape(n, 1)
 
-        x0 = np.exp(-1j * np.angle(vec[0]))
-        vec = np.multiply(vec, x0)
+        x0 = np.exp(1j * np.angle(vec[0]))
+        vec = np.multiply(x0, np.concatenate(vec))
 
         return vec
 
@@ -252,8 +252,8 @@ def EVD_phase_estimation(coh0):
     """ Estimates the phase values based on eigen value decomosition """
     eigen_value, eigen_vector = LA.eigh(coh0, UPLO='U')
     vec = eigen_vector[:, -1].reshape(len(eigen_value), 1)
-    x0 = np.exp(-1j * np.angle(vec[0]))
-    vec = np.multiply(vec, x0)
+    x0 = np.exp(1j * np.angle(vec[0]))
+    vec = np.multiply(x0, np.concatenate(vec))
     return vec
 
 ###############################################################################
@@ -266,8 +266,8 @@ def EMI_phase_estimation(coh0):
         M = np.multiply(LA.pinv(abscoh), coh0)
         eigen_value, eigen_vector = LA.eigh(M, UPLO='U')
         vec = eigen_vector[:, 0].reshape(len(eigen_value), 1)
-        x0 = np.exp(-1j * np.angle(vec[0]))
-        vec = np.multiply(vec, x0)
+        x0 = np.exp(1j * np.angle(vec[0]))
+        vec = np.multiply(x0, np.concatenate(vec))
         return vec
     else:
         print('warning: coherence matrix not positive semidifinite, It is switched from EMI to EVD')