b305718f45ec4c989b6ef26c66e0d52588abe595
Sara Mirzaee <37273875+mirzaees@users.noreply.github.com>
2023-08-22 12:32:23 -0700

    Update version.py

--- src/miaplpy/version.py
+++ src/miaplpy/version.py
@@ -8,6 +8,7 @@ import collections
 ###########################################################################
 Tag = collections.namedtuple('Tag', 'version date')
 release_history = (
+    Tag('0.2.1', '2023-08-22'),
     Tag('0.2.0', '2021-09-14'),
     Tag('0.1.0', '2021-04-23'),
 )
